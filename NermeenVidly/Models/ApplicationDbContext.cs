1ab1e00bc9821efb619e9e063fd994c6bf527ee8
Ammat-ALLAH <ammatallah.ns@gmail.com>
2018-12-27 13:47:07 +0200

    Not recognized Changes

---
+++ NermeenVidly/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace NermeenVidly.Models
+{
+    public class ApplicationDbContext :DbContext
+    {
+
+        DbSet<Customer> customersDBSet { get; set; }
+        public DbSet<Movie> MoviesDBSet { get; set; }
+
+    }
+}
